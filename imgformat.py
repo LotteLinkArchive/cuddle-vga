f71d4ce71e541e1d6c7924e369350de07e2febf0
Naphtha Nepanthez <naphtha@lotte.link>
2020-07-14 15:42:31 +0100

    Switch to with

--- imgformat.py
+++ imgformat.py
@@ -16,22 +16,20 @@ def main():
 	args = parser.parse_args()
 	
 	pic_pixels = Image.open(args.input).convert('RGB').load()
-	output_pic = open(args.output,'w')
 	
-	for y, x in itertools.product(range(args.height), range(args.width)):
-		pixel = pic_pixels[x,y]
-		byte_out = (
-			((pixel[0] & 0b11100000) >> 5) |
-			((pixel[1] & 0b11100000) >> 2) |
-			((pixel[2] & 0b11000000)     )
-		)
+	with open(args.output, 'w') as output_pic:
+		for y, x in itertools.product(range(args.height), range(args.width)):
+			pixel = pic_pixels[x,y]
+			byte_out = (
+				((pixel[0] & 0b11100000) >> 5) |
+				((pixel[1] & 0b11100000) >> 2) |
+				((pixel[2] & 0b11000000)     )
+			)
 
-		output_pic.write(''.join((
-			string.hexdigits[(byte_out & 0b11110000) >> 4],
-			string.hexdigits[(byte_out & 0b00001111)     ], ' '
-		)))
-	
-	output_pic.close()
+			output_pic.write(''.join((
+				string.hexdigits[(byte_out & 0b11110000) >> 4],
+				string.hexdigits[(byte_out & 0b00001111)     ], ' '
+			)))
 	
 if __name__ == '__main__':
 	main()
